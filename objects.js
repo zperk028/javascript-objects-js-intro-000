f5c042adfae970514cb76397d24489bad8911daa
Zachary Perkins <ezmac028@gmail.com>
2020-10-07 17:46:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+
+  return playlist.assign({}, playlist, { [playlist]: songTitle})
+}
